43b3cebd2ea446ca40b542a1648d32332e0c9e14
jeel1637 <jeel34296@gmail.com>
2026-07-31 20:11:57 +0530

    Add global styling and Poppins font

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
